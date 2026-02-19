f57722fa79e85b59ad67135aebb3b87547f6267e
Everton Espedito <evertondev505@gmail.com>
2026-02-19 01:14:15 -0300

    ajustes

--- TaskTracker/src/main/resources/db/migration/V1__create_table_tarefa.sql
+++ TaskTracker/src/main/resources/db/migration/V1__create_table_tarefa.sql
@@ -1,6 +1,6 @@
-    CREATE TABLE tarefa (
-        id SERIAL PRIMARY KEY,
-        titulo VARCHAR(150) NOT NULL,
-        descricao VARCHAR(255) NOT NULL,
-        concluida BOOLEAN NOT NULL DEFAULT FALSE
-    );
+CREATE TABLE tarefa (
+    id BIGSERIAL PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    concluida BOOLEAN DEFAULT FALSE
+);
